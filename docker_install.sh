54a8bd8ebc097a90b45bc71384db08e01f3581ff
safidy3 <safidy.anyaina@gmail.com>
2025-12-07 17:17:41 +0300

    feat: update docker installation script and remove unused scripts

--- docker_install.sh
+++ docker_install.sh
@@ -9,25 +9,31 @@ sudo apt update -y
 
 # Install required packages
 sudo apt install -y \
+	wget gpg apt-transport-https software-properties-common \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
+# VS Code installation
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
 # Add Docker’s official GPG key
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-
 # Set up the Docker repository
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 
 # Update package index again
 sudo apt update -y
 
 # Install Docker Engine, CLI, containerd, and plugins
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install VS Code
+sudo apt install -y code
 
 # Enable and start Docker service
 sudo systemctl enable docker
@@ -47,4 +53,4 @@ sudo docker run hello-world
 echo "Docker installation completed!"
 echo "You may need to log out and back in for group changes to take effect."
 
-make
+make up
